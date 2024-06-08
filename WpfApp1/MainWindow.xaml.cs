cd67e2b57c6ca91e412203f2070137b44881aa64
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2024-06-08 18:09:26 +0300

    update

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,24 +1,76 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private Thread numberThread;
+        private Thread letterThread;
+        private Thread symbolThread;
+        private bool stopThreads;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void StartThreads_Click(object sender, RoutedEventArgs e)
+        {
+            stopThreads = false;
+
+            numberThread = new Thread(GenerateNumbers) { IsBackground = true, Priority = ThreadPriority.Normal };
+            letterThread = new Thread(GenerateLetters) { IsBackground = true, Priority = ThreadPriority.BelowNormal };
+            symbolThread = new Thread(GenerateSymbols) { IsBackground = true, Priority = ThreadPriority.AboveNormal };
+
+            numberThread.Start();
+            letterThread.Start();
+            symbolThread.Start();
+        }
+
+        private void StopThreads_Click(object sender, RoutedEventArgs e)
+        {
+            stopThreads = true;
+
+            numberThread?.Join();
+            letterThread?.Join();
+            symbolThread?.Join();
+        }
+
+        private void GenerateNumbers()
+        {
+            var random = new Random();
+            while (!stopThreads)
+            {
+                int num = random.Next(0, 100);
+                Dispatcher.Invoke(() => OutputTextBox.AppendText($"Number: {num}\n"));
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void GenerateLetters()
+        {
+            var random = new Random();
+            while (!stopThreads)
+            {
+                char letter = (char)random.Next('A', 'Z' + 1);
+                Dispatcher.Invoke(() => OutputTextBox.AppendText($"Letter: {letter}\n"));
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void GenerateSymbols()
+        {
+            var random = new Random();
+            var symbols = "!@#$%^&*()";
+            while (!stopThreads)
+            {
+                char symbol = symbols[random.Next(symbols.Length)];
+                Dispatcher.Invoke(() => OutputTextBox.AppendText($"Symbol: {symbol}\n"));
+                Thread.Sleep(1000);
+            }
         }
     }
 }
